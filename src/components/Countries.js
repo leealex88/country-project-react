d70936c66a8db780c0dbfa469b557d53dd54ca64
ola <olasvoice@gmail.com>
2020-05-21 21:54:56 +0100

    select by Region / first stage

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import CountriesGrid from "./CountriesGrid";
 const Countries = () => {
-  const [countries, setCountries] = useState([]);
+  const [allCountries, setCountries] = useState([]);
+  const [selectedRegion, setSelectedRegion] = useState("Africa");
 
   useEffect(() => {
     fetch(`https://restcountries.eu/rest/v2/all`)
@@ -9,11 +10,27 @@ const Countries = () => {
       .then((data) => setCountries(data));
   }, []);
 
+  const checkTheValue = (e) => {
+    setSelectedRegion(e.target.value);
+  };
+
+  const filterCountriesByRegion = (countries, region) => {
+    return countries.filter((country) => country.region === region);
+  };
   return (
-    <>
-      <select></select>
-      <CountriesGrid countries={countries} />
-    </>
+    <Fragment>
+      <select value={selectedRegion} onChange={checkTheValue}>
+        <option>Filter by Region</option>
+        <option>Africa</option>
+        <option>Americas</option>
+        <option>Asia</option>
+        <option>Europe</option>
+        <option>Oceania</option>
+      </select>
+      <CountriesGrid
+        countries={filterCountriesByRegion(allCountries, selectedRegion)}
+      />
+    </Fragment>
   );
 };
 
