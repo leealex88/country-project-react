3da98eb3f99738154f3bbed864e7cb0500fe8548
ola <olasvoice@gmail.com>
2020-05-21 23:40:04 +0100

    refactoring the fetch / added counter / spliting useEffect

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect, Fragment } from "react";
 import CountriesGrid from "./CountriesGrid";
 const Countries = () => {
-  const [allCountries, setCountries] = useState([]);
+  const [allCountries, setAllCountries] = useState([]);
   const [selectedRegion, setSelectedRegion] = useState("Africa");
+  const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
+  const fetchAndRememberCountries = () => {
     fetch(`https://restcountries.eu/rest/v2/all`)
       .then((res) => res.json())
-      .then((data) => setCountries(data));
-  }, []);
-
-  const checkTheValue = (e) => {
-    setSelectedRegion(e.target.value);
+      .then((data) => setAllCountries(data));
   };
+  useEffect(fetchAndRememberCountries, []);
 
   const filterCountriesByRegion = (countries, region) => {
     return countries.filter((country) => country.region === region);
   };
   return (
     <Fragment>
-      <select value={selectedRegion} onChange={checkTheValue}>
+      <button onClick={() => setCounter(counter + 1)}>Add the count</button>
+      <p>Current value {counter}</p>
+
+      <select
+        value={selectedRegion}
+        onChange={(e) => setSelectedRegion(e.target.value)}
+      >
         <option>Filter by Region</option>
         <option>Africa</option>
         <option>Americas</option>
