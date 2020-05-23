bfc66f610eb9af95eb5c20559d60c78db020a243
ola <olasvoice@gmail.com>
2020-05-23 12:53:13 +0100

    link to each card added / desplaing in new component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Countries from "./components/Countries";
 import Navbar from "./components/Navbar";
-
+import Country from "./components/Country";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Countries />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/country">
+            <Country />
+          </Route>
+          <Route path="/">
+            <Countries />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,27 +1,29 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const CountryCard = ({ country }) => {
   return (
     <div className="xl-col-3 lg-col-3 md-col-3 sm-col-12 col-12">
-      <div className="each-card">
-        <div className="card-flag-container">
-          <img src={country.flag} alt="" />
+      <Link className="link" to={`/country/${country.name}`}>
+        <div className="each-card">
+          <div className="card-flag-container">
+            <img src={country.flag} alt="" />
+          </div>
+          <div className="card-info-container">
+            <p className="country-name">
+              <b>{country.name}</b>
+            </p>
+            <p>
+              <b>Population:</b> {country.population.toLocaleString()}
+            </p>
+            <p>
+              <b>Region:</b> {country.region}
+            </p>
+            <p>
+              <b>Capital:</b> {country.capital}
+            </p>
+          </div>
         </div>
-        <div className="card-info-container">
-          <p className="country-name">
-            <b>{country.name}</b>
-          </p>
-          <p>
-            <b>Population:</b> {country.population.toLocaleString()}
-          </p>
-          <p>
-            <b>Region:</b> {country.region}
-          </p>
-          <p>
-            <b>Capital:</b> {country.capital}
-          </p>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
