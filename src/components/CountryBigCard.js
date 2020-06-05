459ca023957b07b0c62156349a2c9d04e1031b92
ola <olasvoice@gmail.com>
2020-06-05 17:09:09 +0100

    big country card description done / the borders need the functionality

--- src/components/CountryBigCard.js
+++ src/components/CountryBigCard.js
@@ -1,18 +1,21 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, Link } from "react";
 
 const CountryBigCard = (props) => {
   // const { object } = useParams();
   const country = props.location.state.country;
-  console.log("name", country.name);
+  console.log("name", country);
   return (
     <Fragment>
       <div className="each-card">
-        <div className="card-flag-container">
-          <img src={country.flag} alt="" />
+        <div className="big-card-flag">
+          <img src={country.flag} alt="" className="big-card-flag-image" />
         </div>
         <div className="card-info-container">
           <p className="country-name">
             <b>{country.name}</b>
+          </p>
+          <p>
+            <b>Native Name:</b> {country.nativeName}
           </p>
           <p>
             <b>Population:</b> {country.population.toLocaleString()}
@@ -21,8 +24,32 @@ const CountryBigCard = (props) => {
             <b>Region:</b> {country.region}
           </p>
           <p>
+            <b>Seb Region:</b> {country.subregion}
+          </p>
+          <p>
             <b>Capital:</b> {country.capital}
           </p>
+        </div>
+        <div>
+          <p>
+            <b>Top evel Domain:</b> {country.topLevelDomain}
+          </p>
+          <p>
+            <b>Currencies:</b> {country.currencies[0].name}
+          </p>
+          <div>
+            <b>Languages:</b>{" "}
+            {country.languages.map((country, index) => (
+              <p key={index}>{country.name}</p>
+            ))}
+          </div>
+
+          <div>
+            <b>Border Countries:</b>{" "}
+            {country.borders.map((country, index) => (
+              <button key={index}>{country}</button>
+            ))}
+          </div>
         </div>
       </div>
     </Fragment>
